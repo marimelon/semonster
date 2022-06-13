26269e1c8ec4201697e4aab3cfd19ea40e444b92
marimelon <29140293+marimelon@users.noreply.github.com>
2022-06-13 10:33:36 +0900

    Add player feat

--- Main.java
+++ Main.java
@@ -3,5 +3,8 @@ public class Main {
     System.out.println("SEMonster!");
     Monster monster = new Monster();
     System.out.println(monster.toString());
+    var player = new Player();
+    player.drawMonsters();
+    player.showDeck();
   }
 }
